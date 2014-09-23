d498b477c42bfdca13a383155ed87b3785f11687
Jamie Redding <jamieredding2694@gmail.com>
2014-09-23 14:59:13 +0100

    Added nav elements.

--- public_html/index.php
+++ public_html/index.php
@@ -9,8 +9,12 @@
 <body>
 	<div id="site-header">
 		<div id="site-header-container">
+			<h1>FROST</h1>
 			<div id="nav">
-				FROST
+				<ul>
+					<li><a href="#">Most Viewed</a></li>
+					<li><a href="#">Trending</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
