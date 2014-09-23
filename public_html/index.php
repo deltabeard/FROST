2496ea32a6aa29d39709729b6a444951f4498035
deltabeard <mahyar.koshkouei@gmail.com>
2014-09-23 13:14:09 +0100

    Created index.php & banner.css
    Added banner with 'FROST' text.
    Included initial CSS for banner.

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="styles/banner.css">
+<title>FROST</title>
+</head>
+
+<body>
+<div id="site-header">
+	<div id="site-header-container">
+		<div id="nav">
+		FROST
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
