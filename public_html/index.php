b0948c463fc47c8900367ab2b0ebd2c7d1ce4ad1
Jamie Redding <jamieredding2694@gmail.com>
2014-09-23 14:12:25 +0100

    Correct indentation.

--- public_html/index.php
+++ public_html/index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<link rel="stylesheet" href="styles/banner.css">
-<title>FROST</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="styles/banner.css">
+	<title>FROST</title>
 </head>
 
 <body>
-<div id="site-header">
-	<div id="site-header-container">
-		<div id="nav">
-		FROST
+	<div id="site-header">
+		<div id="site-header-container">
+			<div id="nav">
+				FROST
+			</div>
 		</div>
 	</div>
-</div>
-
 </body>
-
 </html>
