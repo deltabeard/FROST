4f057c6d0569e30490ea9cd1f7b93560f3d60f48
Jamie Redding <jamieredding2694@gmail.com>
2014-09-25 13:06:24 +0100

    Added banner to upload.php, moved some padding/margin stuff into border.css

--- public_html/upload.php
+++ public_html/upload.php
@@ -2,18 +2,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="styles/banner.css">
 	<title>FROST - Upload video</title>
 </head>
 
 <body>
-	<div id="site-header">
-		<div id="site-header-container">
-			<div id="nav">
-				FROST
-			</div>
-		</div>
-	</div>
+	<?php
+		include 'banner.html';
+	?>
 
 	<h2>Upload</h2>
 	<form enctype="multipart/form-data" action="upload_file.php" method="POST">
