c181432276a09cba2599979dbf0167e940b30373
Jamie Redding <jamieredding2694@gmail.com>
2014-09-25 11:34:14 +0100

    Moved banner into own file, index.php now 'includes' this.

--- public_html/index.php
+++ public_html/index.php
@@ -2,21 +2,12 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="styles/banner.css">
 	<title>FROST</title>
 </head>
 
 <body>
-	<div id="site-header">
-		<div id="site-header-container">
-			<h1>FROST</h1>
-			<div id="nav">
-				<ul>
-					<li><a href="#">Most Viewed</a></li>
-					<li><a href="#">Trending</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+	<?php
+		include 'banner.html';
+	?>
 </body>
 </html>
