f2a1976e3eb638449afc741fcde9f22d48d4f8b5
Jamie Redding <jamieredding2694@gmail.com>
2014-10-19 20:17:01 +0100

    Reformat upload form into a table for better alignment.

--- public_html/upload.php
+++ public_html/upload.php
@@ -3,23 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>FROST - Upload video</title>
+    <link rel="stylesheet" type="text/css" href="styles/master.css">
 </head>
 
 <body>
 	<?php
 		include 'banner.html';
 	?>
+    <div class="main">
+        <h2>Upload</h2>
+        <form enctype="multipart/form-data" action="upload_file.php" method="POST">
+            <!-- Video information -->
+            <table>
+                <tr>
+                    <td><label for="title">Title: </label></td>
+                    <td><input type="text" name="title"></td>
+                </tr>
+                <tr>
+                    <td><label for="description">Description: </label></td>
+                    <td><textarea maxlength="500" name="description" placeholder="Enter a description of the video here..."></textarea></td>
+                </tr>
+                <tr>
+                    <td><label for="trip_pass">Trip-code password: </label></td>
+                    <td><input type="text" name="trip_pass"></td>
+                </tr>
+                <tr>
+                    <td><label for="uploadtopomf">Upload to pomf.se?</label></td>
+                    <td><input type="checkbox" name="uploadtopomf" value="true"></td>
+                </tr>
+                <tr>
+                    <td><label for="userfile">Upload:</label></td>
+                    <td><input type="file" name="userfile" data-max-size="50MiB"></td>
+                </tr>
+            </table>
 
-	<h2>Upload</h2>
-	<form enctype="multipart/form-data" action="upload_file.php" method="POST">
-		<!-- Video information -->
-		<label for="title">Title: </label><input type="text" name="title"><br>
-		<label for="description">Description: </label><textarea maxlength="500" name="description" placeholder="Enter a description of the video here..."></textarea><br>
-		<label for="trip_pass">Trip-code password: </label><input type="text" name="trip_pass"><br>
-		<input type="checkbox" name="uploadtopomf" value="true"><label for="uploadtopomf">Upload to pomf.se?</label> <br>
-		<label for="userfile">Upload:</label><input type="file" name="userfile" data-max-size="50MiB">
-		<input type="submit" value="Send File">
-	</form>
+            <input type="submit" value="Send File">
+        </form>
+    </div>
 
 </body>
 </html>
