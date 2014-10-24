05785e7865e879f09662996f47ccb5243dd2ae3e
deltabeard <deltabeard@users.noreply.github.com>
2014-10-24 14:46:54 +0100

    Create index.php

---
+++ public_html/management/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>FROST - Management Interface</title>
+	<link rel="stylesheet" type="text/css" href="styles/master.css">
+</head>
+
+<body>
+  This is the FROST Management Interface.
+</body>
+</html>
