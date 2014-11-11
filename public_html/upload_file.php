c675aaf824d86714ea8aa42a6a061c2560c3fd05
Jamie Redding <jamieredding2694@gmail.com>
2014-11-11 15:31:20 +0000

    Turned off error reporting.

--- public_html/upload_file.php
+++ public_html/upload_file.php
@@ -1,4 +1,4 @@
-<?php ini_set('display_errors',1); error_reporting(E_ALL); // Display errors
+<?php //ini_set('display_errors',1); error_reporting(E_ALL); // Display errors
 if ((($_FILES["userfile"]["type"] == "video/webm")  /* <-- This is naive since the type can be faked */
 || ($_FILES["userfile"]["type"] == "video/mp4")     /* We should try using finfo_open */
 || ($_FILES["userfile"]["type"] == "video/ogg")
